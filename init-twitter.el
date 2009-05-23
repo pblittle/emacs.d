4a545d3d308fa442a0add22bf700746df80bd2a3
P. Barrett Little <barrett@designercode.com>
2009-05-23 19:19:38 -0400

    Added twitter directory to store avatars.

--- init-twitter.el
+++ init-twitter.el
@@ -3,5 +3,6 @@
 (add-hook 'twit-new-tweet-hook 'twit-todochiku)
 (setq twit-base-url "https://twitter.com") ;; use https to keep password secure
 (setq twit-show-user-images t)
+(setq twit-user-image-dir "~/.emacs.d/.twitter")
 
 (provide 'init-twitter)
